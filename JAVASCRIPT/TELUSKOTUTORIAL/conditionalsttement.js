0a681465fb0718eb53cc5f965ee2ce7b42812f3c
maainul <mainul080@yahoo.com>
2023-09-17 12:24:06 +0600

    Conditional Statement

---
+++ JAVASCRIPT/TELUSKOTUTORIAL/conditionalsttement.js
@@ -0,0 +1,25 @@
+//  1. Simple Condition
+let n = 7;
+let m = 8;
+let o = 9;
+if (n > m) {
+  console.log("N is greater", n);
+} else {
+  console.log("M is Greater", m);
+}
+
+// 2. 3 Condition
+
+let p = 505;
+let q = 66;
+let r = 7;
+
+if ( p > q && p > r){
+	console.log("P is Greater Value",p);
+}
+else if (q > r){
+	console.log("Q is Greatest Value",q);
+}
+else{
+	console.log("R is Greatest Value",r);
+}
