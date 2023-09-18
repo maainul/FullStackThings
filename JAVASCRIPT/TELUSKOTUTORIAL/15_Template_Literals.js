156dc48a8ceeb10742952de743e96b84e6bae560
maainul <mainul080@yahoo.com>
2023-09-18 10:02:03 +0600

    loops

---
+++ JAVASCRIPT/TELUSKOTUTORIAL/15_Template_Literals.js
@@ -0,0 +1,12 @@
+let num1 = 7;
+let num2 = 4;
+let result = num1 + num2;
+
+console.log("The addition of num1 and num2 is = ", result);
+
+console.log("The addition of " + num1 + " and " + num2 + " is = ", result);
+
+// using template literals
+console.log(
+  `The addition of ${num1} and ${num2} is ${result}`
+);
---
+++ JAVASCRIPT/TELUSKOTUTORIAL/16_While_loop.js
@@ -0,0 +1,21 @@
+let n = 1;
+while (n <= 7) {
+  console.log("HI");
+  n += 1;
+}
+
+for (let i = 1; i <= 5; i++) {
+  console.log(`HI ${i}`);
+}
+/*
+for (;;) {
+  console.log(hi);
+}
+*/
+// While and  For Lop
+
+let num = 564782;
+while (num > 0) {
+  console.log(num % 10);
+  num = parseInt(num / 10);
+}
