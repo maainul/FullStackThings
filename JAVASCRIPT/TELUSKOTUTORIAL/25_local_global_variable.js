fd240a0717cf511c2c7c5b8b316fec2358328ecf
maainul <mainul080@yahoo.com>
2023-09-19 09:51:23 +0600

    arrow function

---
+++ JAVASCRIPT/TELUSKOTUTORIAL/25_local_global_variable.js
@@ -0,0 +1,9 @@
+function greetpass(data) {
+    console.log(num);
+    return data;
+  }
+  let user="Mainul"
+  let num = 5;
+  let str = greetpass(user);
+  console.log(str);
+  
---
+++ JAVASCRIPT/TELUSKOTUTORIAL/26_arrow_function.js
@@ -0,0 +1,11 @@
+let greet = (data) => {
+  return data;
+};
+
+console.log(greet("Mainul"));
+
+let pass = ()=>{
+    console.log('Pass by value');
+}
+
+pass();
