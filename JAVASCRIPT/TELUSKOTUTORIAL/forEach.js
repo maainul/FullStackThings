3b681a0c4e7cd83a7f342f4c2e951a4240e275a0
maainul <mainul080@yahoo.com>
2023-09-23 12:12:44 +0600

    For each loop in js

---
+++ JAVASCRIPT/TELUSKOTUTORIAL/forEach.js
@@ -0,0 +1,7 @@
+let nums = [42, 51, 24, 98, 65, 12];
+console.log(nums);
+// forEach operator will take one function . that function can do anything with this nums value.
+
+nums.forEach((n) => {
+  console.log(n);
+});
