d2855f1de16b5e57fe190be2e6cf16afdc3122d7
maainul <mainul080@yahoo.com>
2023-10-23 05:10:18 +0600

    Post Method in Express.Install Middleware and install colors,morgan

--- Nodejs/learning-expressjs/README.md
+++ Nodejs/learning-expressjs/README.md
@@ -28,4 +28,14 @@
 
         npm run server (nodemon run the project : "server": "nodemon server.js")
         
+6. install morgan middle : it will give extra data about api : like 
+```
+Server Running On 5000
+POST /contact-form-json 200 3.762 ms - 49
+```
+        npm i morgan
+
+7. Colors package for color terminal :
+
+        npm i colors
 
--- Nodejs/learning-expressjs/server.js
+++ Nodejs/learning-expressjs/server.js
@@ -1,18 +1,38 @@
 // import
 const express = require("express");
-
+const morgan = require("morgan");
+const colors = require("colors");
 //rest object
 const app = express();
+
+//middlewares
+app.use(express.json()) // Parse into json data through middleware from express js framework
+app.use(morgan('dev'))
 
 // routes
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to the world</h1>");
 });
 
+//post
+app.post('/contact-form-normal',(req,res)=>{
+    res.send("Form Data Submitted");
+})
+
+//send through json object
+app.post("/contact-form-json", (req, res) => {
+  const {inputData} = req.body;
+    res.json({
+    success:true,
+    message:`Welcome ${inputData}`
+  });
+});
+
+
 //port number
 const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server Running On ${PORT}`);
+  console.log(`Server Running On ${PORT}`.bgMagenta);
 });
 
