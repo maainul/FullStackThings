0a671411a20659c7325e408a5c10b0a017b1129b
maainul <mainul080@yahoo.com>
2023-11-13 09:13:38 +0600

    redux

---
+++ REACT/redux-example/src/demo.js
@@ -0,0 +1,39 @@
+// imports
+const {createStore} = require('redux');
+
+// initial state
+const initialState = {
+    bal : 5000,
+}
+
+
+// reducer function 
+const reducer = (state= initialState,action) =>{
+
+    switch (action.type) {
+        case 'DEPOSIT':
+            return{
+                ...state,
+                bal : action.value
+            };
+            case 'WITHDRAW':
+                return{
+                    ...state,
+                    bal :action.value
+                };
+
+            default :
+            state;
+    }
+
+}
+
+// store
+const store = createStore(reducer);
+store.subscribe(() =>{
+    console.log(store.getState());
+})
+
+// action
+store.dispatch({ type: "DEPOSIT", value: 1000 });
+store.dispatch({type:'WITHDRAW',value:4000});
