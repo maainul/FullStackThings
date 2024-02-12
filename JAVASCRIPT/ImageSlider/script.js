56ae1d8b94252464705c4e4779112a38b5ffaeaa
Md.Mainul Hasan <mdevhasan@gmail.com>
2024-02-12 16:40:04 +0600

    Image Slicer Start

---
+++ JAVASCRIPT/ImageSlider/script.js
@@ -0,0 +1,27 @@
+const slides = document.querySelectorAll(".slides img")
+console.log(slides)
+
+let slideIndex = 0;
+let intervalId = null
+
+
+
+initializeSlider();
+
+function initializeSlider () {
+    slides[slideIndex].classList.add("displaySlide")
+}
+
+function showSlide(index){
+
+}
+
+
+function prevSlide(){
+
+}
+
+
+function nextSlide(){
+
+}
